8558aee365ff2a6f1fc5c444818dc012a4fbd00b
윤호 채 <yoonho2011@gmail.com>
2020-10-07 17:49:31 +0900

    oK

---
+++ WebApplication1/App_Start/BundleConfig.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Web.Optimization
+
+Public Module BundleConfig
+    ' 묶음에 대한 자세한 내용은 https://go.microsoft.com/fwlink/?LinkId=301862를 참조하세요.
+    Public Sub RegisterBundles(ByVal bundles As BundleCollection)
+
+        bundles.Add(New ScriptBundle("~/bundles/jquery").Include(
+                    "~/Scripts/jquery-{version}.js"))
+
+        bundles.Add(New ScriptBundle("~/bundles/jqueryval").Include(
+                    "~/Scripts/jquery.validate*"))
+
+        ' Modernizr의 개발 버전을 사용하여 개발하고 배우십시오. 그런 다음
+        ' 프로덕션에 사용할 준비를 하고 https://modernizr.com의 빌드 도구를 사용하여 필요한 테스트만 선택하세요.
+        bundles.Add(New ScriptBundle("~/bundles/modernizr").Include(
+                    "~/Scripts/modernizr-*"))
+
+        bundles.Add(New ScriptBundle("~/bundles/bootstrap").Include(
+                  "~/Scripts/bootstrap.js"))
+
+        bundles.Add(New StyleBundle("~/Content/css").Include(
+                  "~/Content/bootstrap.css",
+                  "~/Content/site.css"))
+    End Sub
+End Module
+
